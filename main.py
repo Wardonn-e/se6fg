b4bfce4c95102fb0d6c67236ed17c393031763d4
wardonne <Wardonne@yandex.ru>
2023-10-31 20:18:50 +0400

    bgk

---
+++ main.py
@@ -0,0 +1,18 @@
+import concurrent.futures
+def calculate_row_sum(row):
+    return sum(row)
+def main():
+    matrix_size = int(input("Введите размер матрицы: "))
+    matrix = []
+    for i in range(matrix_size):
+        row = list(map(int, input(f"Введите значения {i+1}-й строки через пробел: ").split()))
+        matrix.append(row)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        row_sums = []
+        for row in matrix:
+            future = executor.submit(calculate_row_sum, row)
+            row_sums.append(future)
+        total_row_sum = sum(future.result() for future in row_sums)
+        print("Сумма элементов по строкам:", total_row_sum)
+if __name__ == "__main__":
+    main()
